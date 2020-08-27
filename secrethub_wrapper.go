24adca651be1f9430f765153d535da225e53d038
Marton Soos <marton.soos@secrethub.io>
2020-08-27 15:15:44 +0200

    Add error handling workaround

--- secrethub_wrapper.go
+++ secrethub_wrapper.go
@@ -53,3 +53,28 @@ func ResolveEnv() (map[string]string, error) {
 	}
 	return resolvedEnv, nil
 }
+
+// Workaround for error handling.
+
+// errMessage stores the message of the last encountered error.
+// If no error was encountered it is "".
+var errMessage string
+
+// setErr stores the message of the given error in errMessage,
+// so it can be checked from the wrapper C code.
+func setErr(err error) {
+	errMessage = err.Error()
+}
+
+// checkErr returns the message of the last encountered error.
+// It is called from the C wrapper code.
+//export checkErr
+func checkErr() *C.char {
+	return C.CString(errMessage)
+}
+
+// clearErr clears errMessage.
+//export clearErr
+func clearErr() {
+	errMessage = ""
+}
