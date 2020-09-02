8b7f93c1b3408363cc9fa38a335d843ae5635787
Marton Soos <marton.soos@secrethub.io>
2020-09-02 16:35:17 +0200

    Delete unneded file

--- output/build.sh
+++ output/build.sh
@@ -3,4 +3,4 @@ swig -csharp ./secrethub.i
 gcc -O2 -fPIC -c secrethub_wrap.c
 gcc -shared  secrethub_wrap.o secrethub.a  -o libsecrethub.so
 mono-csc -out:runme.exe *.cs
-rm secrethub.a secrethub_wrap.o secrethub_wrap.c secrethub.h secrethub.cs secrethubPINVOKE.cs SecretVersion.cs
+rm secrethub.a secrethub_wrap.o secrethub_wrap.c secrethub.h secrethub.cs secrethubPINVOKE.cs SecretVersion.cs Secret.cs
