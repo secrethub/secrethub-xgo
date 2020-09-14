7cfe30bd6d891f09eb78fa1f2bae5de27daeb340
Marton Soos <marton.soos@secrethub.io>
2020-09-14 10:20:02 +0200

    Revert "Use Resolve and ResolveEnv from secrethub-go SDK"
    We decided to postpone adding these functions to the secrethub-go SDK.
    This reverts commit 99a722f5ad9843357cff184353413dd1bc7862d3.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/secrethub/secrethub-xgo
 
 go 1.14
 
-require github.com/secrethub/secrethub-go v0.30.1-0.20200911095301-a8aedebf28bd
+require github.com/secrethub/secrethub-go v0.30.0
--- secrethub_wrapper.go
+++ secrethub_wrapper.go
@@ -31,6 +31,7 @@ import "C"
 import (
 	"encoding/json"
 	"os"
+	"strings"
 
 	"github.com/secrethub/secrethub-go/pkg/secrethub"
 )
@@ -105,12 +106,16 @@ func Resolve(ref *C.char, errMessage **C.char) *C.char {
 		*errMessage = C.CString(err.Error())
 		return nil
 	}
-	resolvedRef, err := client.Secrets().Resolve(C.GoString(ref))
-	if err != nil {
-		*errMessage = C.CString(err.Error())
-		return nil
+	bits := strings.Split(C.GoString(ref), "://")
+	if len(bits) == 2 && bits[0] == "secrethub" {
+		secret, err := client.Secrets().Read(bits[1])
+		if err != nil {
+			*errMessage = C.CString(err.Error())
+			return nil
+		}
+		return C.CString(string(secret.Data))
 	}
-	return C.CString(string(resolvedRef))
+	return ref
 }
 
 // ResolveEnv takes a map of environment variables and replaces the values of those
@@ -118,22 +123,18 @@ func Resolve(ref *C.char, errMessage **C.char) *C.char {
 // of the respective secret. The other entries in the map remain untouched.
 //export ResolveEnv
 func ResolveEnv(errMessage **C.char) *C.char {
-	client, err := Client()
-	if err != nil {
-		*errMessage = C.CString(err.Error())
-		return nil
-	}
-	resolvedEnv, err := client.Secrets().ResolveEnv(os.Environ())
-	if err != nil {
-		*errMessage = C.CString(err.Error())
-		return nil
+	envVars := os.Environ()
+	resolvedEnv := make(map[string]string, len(envVars))
+	for _, value := range envVars {
+		keyValue := strings.Split(value, "=")
+		resolvedEnv[keyValue[0]] = C.GoString(Resolve(keyValue[1], errMessage))
 	}
 	encoding, err := json.Marshal(resolvedEnv)
 	if err != nil {
 		*errMessage = C.CString(err.Error())
 		return nil
 	}
-	return C.CString(string(encoding))
+	return C.CString(encoding)
 }
 
 // Exists checks if a secret exists at `path`.
