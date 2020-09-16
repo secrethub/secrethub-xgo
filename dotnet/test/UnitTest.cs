d9eee3652f2913d1dcbe4c6273e3c940a5f4a0c9
Eduard Filip <eduard.filip@secrethub.io>
2020-09-16 16:20:36 +0200

    Add extra test case with more equal signs

--- dotnet/test/UnitTest.cs
+++ dotnet/test/UnitTest.cs
@@ -50,11 +50,13 @@ namespace SecretHubTest
             Assert.Equal("super_secret_value", SecretHub.Client.Resolve("secrethub://secrethub-xgo/dotnet/test-secret"));  
         }
 
-        [Fact]
-        public void TestResolveEnvSuccess() {
+        [Theory]
+        [InlineData("super_secret_value", "TEST")]
+        [InlineData("aaaa", "OTHER_TEST")]
+        [InlineData("this=has=three=equals", "TEST_MORE_EQUALS")]
+        public void TestResolveEnvSuccess(string secretValue, string envVarName) {
             System.Collections.Generic.IDictionary<string,string> res = SecretHub.Client.ResolveEnv();
-            Assert.Equal("super_secret_value", res["TEST"]);
-            Assert.Equal("aaaa", res["OTHER_TEST"]);
+            Assert.Equal(secretValue, res[envVarName]);
         }
 
         [Theory]
